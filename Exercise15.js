fb17843383ebb31301b5e8da0059e02beb8e23ba
Jordan Parker <parkerj4321@gmail.com>
2018-07-31 13:15:00 -0500

    Exercise 15

---
+++ Exercise15.js
@@ -0,0 +1,11 @@
+function main(){
+    var yourNum = parseInt(prompt("What's your number"));
+    favoriteNumber(yourNum);
+
+}
+function favoriteNumber(number) {
+    alert(number);
+
+}
+
+main();
